28d0ee898675b7e3cdee3848be5dc880b234916b
ewankdo78 <146921882+ewankdo78@users.noreply.github.com>
2023-10-06 11:17:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let aQ="jean";
+let bBis="Paul";
+let result=checkName(a,b)
+
+function checkName(name1, name2){
+    if(name1===name2){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(result);
